1d3d7d34585428e8c43bb6671edaa7cb7c245fa3
Muhamed Zeqiri <34819956+Muhamedii@users.noreply.github.com>
2019-04-30 14:03:47 +0200

    Add project files.

---
+++ Implementimi-I-BASE32-Enkoderit-Dekoderit/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Implementimi_I_BASE32_Enkoderit_Dekoderit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDecodedText = new System.Windows.Forms.TextBox();
+            this.txtEncodedtext = new System.Windows.Forms.TextBox();
+            this.txtPlaintext = new System.Windows.Forms.TextBox();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(505, 245);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(108, 35);
+            this.btnDecode.TabIndex = 15;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Decoded Text";
+            // 
+            // txtDecodedText
+            // 
+            this.txtDecodedText.Location = new System.Drawing.Point(148, 297);
+            this.txtDecodedText.Name = "txtDecodedText";
+            this.txtDecodedText.Size = new System.Drawing.Size(465, 22);
+            this.txtDecodedText.TabIndex = 13;
+            // 
+            // txtEncodedtext
+            // 
+            this.txtEncodedtext.Location = new System.Drawing.Point(148, 180);
+            this.txtEncodedtext.MinimumSize = new System.Drawing.Size(270, 22);
+            this.txtEncodedtext.Name = "txtEncodedtext";
+            this.txtEncodedtext.Size = new System.Drawing.Size(465, 22);
+            this.txtEncodedtext.TabIndex = 12;
+            // 
+            // txtPlaintext
+            // 
+            this.txtPlaintext.Location = new System.Drawing.Point(148, 80);
+            this.txtPlaintext.Name = "txtPlaintext";
+            this.txtPlaintext.Size = new System.Drawing.Size(465, 22);
+            this.txtPlaintext.TabIndex = 11;
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Location = new System.Drawing.Point(505, 124);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(108, 35);
+            this.btnEncode.TabIndex = 10;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Encoded Text";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Plaintext";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 404);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDecodedText);
+            this.Controls.Add(this.txtEncodedtext);
+            this.Controls.Add(this.txtPlaintext);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Base32 Enkoderi/Dekoderi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDecodedText;
+        private System.Windows.Forms.TextBox txtEncodedtext;
+        private System.Windows.Forms.TextBox txtPlaintext;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Implementimi-I-BASE32-Enkoderit-Dekoderit/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace Implementimi_I_BASE32_Enkoderit_Dekoderit
+{
+    public partial class Form1 : Form
+    {
+
+        public byte[] inputibytes;
+        public string inputi;
+        public string TekstiEnkoduar;
+        public string TekstiDekoduar;
+        private const int MadhesiaBajtit = 8;
+        private const int BllokuDales = 5;
+        private const string AlfabetiBASE32 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("KUJDES!\n-Per dekodim hiqni karakteret qe perdoren per padding '='\n-Paddingu nuk ndryshon permbajtjen\n");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+
+        {
+            inputi = txtPlaintext.Text;
+            inputibytes = Encoding.ASCII.GetBytes(inputi.ToString());
+            TekstiEnkoduar = Enkodo(inputibytes);
+            txtEncodedtext.Text = TekstiEnkoduar;
+
+        }
+        static string Enkodo(byte[] bytes)
+        {
+
+            if (bytes == null)
+            {
+                return null;
+            }
+
+            else if (bytes.Length == 0)
+            {
+                return string.Empty;
+            }
+
+
+            StringBuilder builder = new StringBuilder(bytes.Length * MadhesiaBajtit / BllokuDales);
+
+
+            int bytesPosition = 0;
+
+
+
+            int bytesSubPosition = 0;
+
+
+            byte outputBase32Byte = 0;
+
+
+            int outputBase32BytePosition = 0;
+
+
+            while (bytesPosition < bytes.Length)
+            {
+
+                int bitsAvailableInByte = Math.Min(MadhesiaBajtit - bytesSubPosition, BllokuDales - outputBase32BytePosition);
+
+
+                outputBase32Byte <<= bitsAvailableInByte;
+
+
+                outputBase32Byte |= (byte)(bytes[bytesPosition] >> (MadhesiaBajtit - (bytesSubPosition + bitsAvailableInByte)));
+
+
+                bytesSubPosition += bitsAvailableInByte;
+
+
+                if (bytesSubPosition >= MadhesiaBajtit)
+                {
+
+                    bytesPosition++;
+                    bytesSubPosition = 0;
+                }
+
+
+                outputBase32BytePosition += bitsAvailableInByte;
+
+
+                if (outputBase32BytePosition >= BllokuDales)
+                {
+
+                    outputBase32Byte &= 0x1F;
+
+
+                    builder.Append(AlfabetiBASE32[outputBase32Byte]);
+
+
+                    outputBase32BytePosition = 0;
+                }
+            }
+
+
+            if (outputBase32BytePosition > 0)
+            {
+
+                outputBase32Byte <<= (BllokuDales - outputBase32BytePosition);
+
+
+                outputBase32Byte &= 0x1F;
+
+
+                builder.Append(AlfabetiBASE32[outputBase32Byte]);
+            }
+
+            if ((bytes.Length * MadhesiaBajtit) % BllokuDales == 0)
+            {
+                return builder.ToString();
+            }
+
+
+            else if (bytes.Length < 5)
+            {
+                int paddingcount = Math.Abs((int)((bytes.Length * MadhesiaBajtit) / BllokuDales) - 8 + 1);
+
+                List<string> list = new List<string>();
+                for (int i = 0; i < paddingcount; i++)
+                {
+                    list.Add("=");
+                }
+
+                string paddingString = string.Join("", list.ToArray());
+                return builder.ToString() + paddingString;
+
+            }
+            else if (bytes.Length > 5 && bytes.Length < 10)
+            {
+                int paddingcount = Math.Abs((int)((bytes.Length * MadhesiaBajtit) / BllokuDales) - 16 + 1);
+
+                List<string> list = new List<string>();
+                for (int i = 0; i < paddingcount; i++)
+                {
+                    list.Add("=");
+                }
+
+                string paddingString = string.Join("", list.ToArray());
+                return builder.ToString() + paddingString;
+            }
+
+
+
+            else
+            {
+                return builder.ToString();
+            }
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
